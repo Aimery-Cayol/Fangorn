07fb36db4f746a7016e6406866909661682d4d1c
Aimery Cayol <aimerycayol@gmail.com>
2021-02-02 11:05:56 +0100

    ca va secouer

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  resources :contacts
+  get 'home/index'
+  resources :posts
+  get 'blags/index'
+  get 'blags/show'
+  get 'blags/new'
+  get 'blags/create'
+  get 'blags/edit'
+  get 'blags/update'
+  get 'blags/destroy'
+  
+  root 'home#index'
+
+  resources :articles
+  resources :blags
+  resources :users
+  get '/users/create', to: 'users#create', as: 'usercrea'
+
+end
