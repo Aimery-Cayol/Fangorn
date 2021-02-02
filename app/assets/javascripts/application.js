35633e91a4a4bb5e2ba3151b5274e691a77723e5
Aimery Cayol <aimerycayol@gmail.com>
2021-02-02 12:16:14 +0100

    compiling images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,3 +20,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require material/scripts
+
+
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+});
+});
+
