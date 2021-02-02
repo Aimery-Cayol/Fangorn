50cf80c56a63792a82e33c948156bdf86f201e11
Aimery Cayol <aimerycayol@gmail.com>
2021-02-02 11:43:03 +0100

    everything up to date, dealing with flash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_094414) do
+ActiveRecord::Schema.define(version: 2021_02_02_103546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_02_02_094414) do
   create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
   create_table "posts", force: :cascade do |t|
